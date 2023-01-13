9e2d9da5b6a83e386b0c0474bb790c07650e262f
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-13 09:54:27 -0800

    Test: hospital show page lists docs patient coutnt

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -43,6 +43,31 @@ RSpec.describe 'hospital show page' do
 
     expect(page).to have_no_content(doc3.name)
   end
-  it 'shows number of patients next to each doctor'
+
+  it 'shows number of patients next to each doctor and orders docs by patient count' do
+    hospital = Hospital.create!(name: "University of California San Diego La Jolla")
+    doc1 = hospital.doctors.create!(name: "Jessica Schulte", specialty: "Nuero-oncology", university: "Northwestern University Feinberg School of Medicine")
+    doc2 = hospital.doctors.create!(name: "Anhnhi Tran", specialty: "Physical Therapy", university: "University of South Florida")
+    patient1 = Patient.create!(name: 'John Doe', age: 70)
+    patient2 = Patient.create!(name: 'Jane Doe', age: 40)
+    patient3 = Patient.create!(name: 'Emma Doe', age: 50)
+    patient4 = Patient.create!(name: 'Alex Doe', age: 60)
+    patient5 = Patient.create!(name: 'Tim Cook', age: 60)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient1.id)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient2.id)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient3.id)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient4.id) 
+    DoctorPatient.create!(doctor_id: doc2.id, patient_id: patient1.id) 
+    
+    visit hospital_path(hospital)
+
+    expect(doc1.name).to appear_before(doc2.name)
+    within ("#doctor-#{doc1.id}") do
+      expect(page).to have_content("Patients: #{doc1.patient_count}")
+    end
+    within ("#doctor-#{doc2.id}") do
+      expect(page).to have_content("Patients: #{doc2.patient_count}")
+    end
+  end
   it 'lists doctors ordered by patient count'
 end
