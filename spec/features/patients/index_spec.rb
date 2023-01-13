8335a4e0c1fb82de8227239949a730434c55df98
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-13 09:24:30 -0800

    Test: patients index page

--- spec/features/patients/index_spec.rb
+++ spec/features/patients/index_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'patients index page' do
     patient3 = Patient.create!(name: 'Emma Doe', age: 50)
     patient4 = Patient.create!(name: 'Alex Doe', age: 60)
     patient5 = Patient.create!(name: 'Tim Cook', age: 60)
+    patient6 = Patient.create!(name: 'Jim Cook', age: 10)
     # DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient1.id)
     # DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient2.id)
     # DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient3.id)
@@ -18,12 +19,14 @@ RSpec.describe 'patients index page' do
 
     visit patients_path
 
+    expect(page).to have_content("Adult patients:")
     expect(patient4.name).to appear_before(patient3.name)
     expect(patient3.name).to appear_before(patient2.name)
     expect(patient2.name).to appear_before(patient1.name)
     expect(patient1.name).to appear_before(patient5.name)
     expect(page).to have_content(Patient.adults_sorted_alphabetically[0].name)
     expect(page).to have_content(Patient.adults_sorted_alphabetically[-1].name)
+    expect(page).to have_no_content(patient6.name)
   end
 #   User Story 3, Patient Index Page
 # ​
