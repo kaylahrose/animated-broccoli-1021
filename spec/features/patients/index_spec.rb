d0d30adccd4c5b82ec3882fe38cc8a018d04fd19
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-13 09:25:44 -0800

    Reformat

--- spec/features/patients/index_spec.rb
+++ spec/features/patients/index_spec.rb
@@ -2,20 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'patients index page' do
   it 'lists names of all adult patients in ascending alphabetical order' do
-    # hospital = Hospital.create!(name: "University of California San Diego La Jolla")
-    # doc1 = hospital.doctors.create!(name: "Jessica Schulte", specialty: "Nuero-oncology", university: "Northwestern University Feinberg School of Medicine")
-    # doc2 = hospital.doctors.create!(name: "Anhnhi Tran", specialty: "Physical Therapy", university: "University of South Florida")
     patient1 = Patient.create!(name: 'John Doe', age: 70)
     patient2 = Patient.create!(name: 'Jane Doe', age: 40)
     patient3 = Patient.create!(name: 'Emma Doe', age: 50)
     patient4 = Patient.create!(name: 'Alex Doe', age: 60)
     patient5 = Patient.create!(name: 'Tim Cook', age: 60)
     patient6 = Patient.create!(name: 'Jim Cook', age: 10)
-    # DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient1.id)
-    # DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient2.id)
-    # DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient3.id)
-    # DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient4.id) 
-    # DoctorPatient.create!(doctor_id: doc2.id, patient_id: patient1.id) 
 
     visit patients_path
 
@@ -28,11 +20,4 @@ RSpec.describe 'patients index page' do
     expect(page).to have_content(Patient.adults_sorted_alphabetically[-1].name)
     expect(page).to have_no_content(patient6.name)
   end
-#   User Story 3, Patient Index Page
-# ​
-# As a visitor
-# When I visit the patient index page
-# I see the names of all adult patients (age is greater than 18),
-# And I see the names are in ascending alphabetical order (A - Z, you do not need to account for capitalization)
-
 end
