8533fbaaeaa8c7461e58c19728dc934bfa2bf46f
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-13 09:21:18 -0800

    Feat: adult patients sorted alphabetically

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
+
+  def self.adults_sorted_alphabetically
+    Patient.where("age >= ?", 17).order(:name)
+  end
 end
