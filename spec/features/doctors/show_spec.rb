23a518bd20ca7b2d778a4bd2f0ba5c342ac7e778
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-13 08:42:51 -0800

    Test: remove patient from doc caseload

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -26,4 +26,48 @@ RSpec.describe 'doctor show page' do
     expect(page).to have_content(patient4.name)
     expect(page).to have_no_content(patient5.name)
   end
+
+  it 'removes a patient from a doctor' do
+#     User Story 2, Remove a Patient from a Doctor
+# ​
+# As a visitor
+# When I visit a Doctor's show page
+# Then next to each patient's name, I see a button to remove that patient from that doctor's caseload
+# When I click that button for one patient
+# I'm brought back to the Doctor's show page
+# And I no longer see that patient's name listed
+# And when I visit a different doctor's show page that is caring for the same patient,
+# Then I see that the patient is still on the other doctor's caseload
+    hospital = Hospital.create!(name: "University of California San Diego La Jolla")
+    doc1 = hospital.doctors.create!(name: "Jessica Schulte", specialty: "Nuero-oncology", university: "Northwestern University Feinberg School of Medicine")
+    doc2 = hospital.doctors.create!(name: "Anhnhi Tran", specialty: "Physical Therapy", university: "University of South Florida")
+    patient1 = Patient.create!(name: 'John Doe', age: 70)
+    patient2 = Patient.create!(name: 'Jane Doe', age: 40)
+    patient3 = Patient.create!(name: 'Emma Doe', age: 50)
+    patient4 = Patient.create!(name: 'Alex Doe', age: 60)
+    patient5 = Patient.create!(name: 'Tim Cook', age: 60)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient1.id)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient2.id)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient3.id)
+    DoctorPatient.create!(doctor_id: doc1.id, patient_id: patient4.id) 
+    DoctorPatient.create!(doctor_id: doc2.id, patient_id: patient1.id) 
+
+    visit doctor_path(doc2)
+    expect(page).to have_content(patient1.name)
+
+    visit doctor_path(doc1)
+    expect(page).to have_content(patient1.name)
+    expect(page).to have_content(patient2.name)
+
+    within("#patient-#{patient1.id}") do
+      click_button 'Remove'
+    end
+    
+    expect(current_path).to eq(doctor_path(doc))
+    expect(page).to have_no_content(patient1.name)
+    expect(page).to have_content(patient2.name)
+    
+    visit doctor_path(doc2)
+    expect(page).to have_content(patient1.name)
+  end
 end
