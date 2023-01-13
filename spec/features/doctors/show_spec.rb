781f658fee23cd69b548abbf6e3aa00ceec0dbac
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-13 08:36:01 -0800

    Update: doctor show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'doctor show page' do
   it 'lists doctors attributes, hospital, and lists patients' do
-    hospital = Hospital.create!(name: "Sloan Grey Memorial")
-    doc = hospital.doctors.create!(name: "Jessica Schulte", specialty: "Nuero-oncology", university: "UCSF")
+    hospital = Hospital.create!(name: "University of California San Diego La Jolla")
+    doc = hospital.doctors.create!(name: "Jessica Schulte", specialty: "Nuero-oncology", university: "Northwestern University Feinberg School of Medicine")
     patient1 = Patient.create!(name: 'John Doe', age: 70)
     patient2 = Patient.create!(name: 'Jane Doe', age: 40)
     patient3 = Patient.create!(name: 'Emma Doe', age: 50)
@@ -18,21 +18,12 @@ RSpec.describe 'doctor show page' do
 
     expect(page).to have_content("Dr. #{doc.name}")
     expect(page).to have_content("Specialty: #{doc.specialty}")
-    expect(page).to have_content("University: #{doc.university}")
+    expect(page).to have_content("Medical Degree from: #{doc.university}")
+    expect(page).to have_content("Working at: #{doc.hospital.name}")
     expect(page).to have_content(patient1.name)
     expect(page).to have_content(patient2.name)
     expect(page).to have_content(patient3.name)
     expect(page).to have_content(patient4.name)
     expect(page).to have_no_content(patient5.name)
   end
-#   User Story 1, Doctors Show Page
-# ​
-# As a visitor
-# When I visit a doctor's show page
-# I see all of that doctor's information including:
-#  - name
-#  - specialty
-#  - university where they got their doctorate
-# And I see the name of the hospital where this doctor works
-# And I see the names of all of the patients this doctor has
 end
